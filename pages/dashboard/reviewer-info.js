25225194370910036335476a969ad93cf5c8a2af
Venkadesh S <venkadesh@kissflow.com>
2023-10-07 12:04:58 +0530

    intial pr dashboard setup

---
+++ pages/dashboard/reviewer-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function ReviewersList() {
+  return (
+    <div>
+      <div>reviewers info</div>
+      <Link href="/dashboard/reviewers-list">Back to reviewers info</Link>
+    </div>
+  );
+}
---
+++ pages/dashboard/reviewers-list.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import Link from "next/link";
+
+//tremor components
+import { Card, ProgressBar } from "@tremor/react";
+
+import { MOCK_DATA } from "../mockdata";
+
+export default function ReviewersList() {
+  const [reviewersList, setReviewersList] = useState(MOCK_DATA);
+  return (
+    <div>
+      <div>reviewers list</div>
+      <Link href="/">Back to home</Link>
+      {reviewersList.map((reviewersInfo) => (
+        <Card className="max-w-xs mx-auto">
+          <Link href="/dashboard/reviewer-info">info card</Link>
+          <ProgressBar value={32} className="mt-2" />
+          <span>{reviewersInfo.name}</span>
+        </Card>
+      ))}
+    </div>
+  );
+}
