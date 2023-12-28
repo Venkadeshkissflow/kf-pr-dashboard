5de2ded4602a203126d9f7f098b4f7bc035fbc3b
Venkadesh S <venkadesh@kissflow.com>
2023-12-28 19:29:16 +0530

    new layout search behaviour done

--- src/app/components/Card/index.jsx
+++ src/app/components/Card/index.jsx
@@ -1,22 +1,17 @@
 "use client";
 
 import Image from "next/image";
+import PropTypes from "prop-types";
 
 import { Card } from "@tremor/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
-export default function ReviewerInfoCard({
-  reviewerInfo,
-  isClickable,
-  userProfile,
-}) {
-  const { name, totalReviewedPr, avgReviewTime, profileUrl, avatarUrl } =
-    reviewerInfo;
-  const router = useRouter();
+ReviewerInfoCard.PropTypes = {
+  title: PropTypes.string,
+  avatar: PropTypes.string,
+};
 
+export default function ReviewerInfoCard({ title, avatar }) {
   function handleOnClick() {
-    // if (isClickable) router.push(`./reviewerinfo/${reviewerInfo.id}`);
     console.log("clicked");
   }
 
@@ -25,20 +20,12 @@ export default function ReviewerInfoCard({
       <Card onClick={handleOnClick} className="animate-swipeIn flex gap-4 p-2">
         <Image
           style={{ borderRadius: "50%" }}
-          src={userProfile}
+          src={avatar}
           width={70}
           height={70}
           alt="Author profile"
         />
-        <div className="grid grid-cols-3 w-full">
-          <div className="col-span-3 font-semibold text-l">{name}</div>
-          {/* <div className="col-span-1 font-normal text-base">
-          Total reviewed pr: <span>{totalReviewedPr}</span>
-        </div>
-        <div className="col-span-1 font-normal text-base">
-          Average reviewed time : <span>{avgReviewTime}</span>
-        </div> */}
-        </div>
+        <div className="flex items-center">{title}</div>
       </Card>
     </div>
   );
