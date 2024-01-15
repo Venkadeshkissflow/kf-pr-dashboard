76d57706deaf44f4e7664202f5cf8d2f87785c1b
Venkadesh S <venkadesh@kissflow.com>
2024-01-15 21:57:08 +0530

    Contributors table with mock data

--- src/app/(screens)/dashboard/@commoninfo/page.js
+++ src/app/(screens)/dashboard/@commoninfo/page.js
@@ -1,4 +1,5 @@
 // import { PERIOD_BASE_FILTER } from "@/app/common";
+import { TopReviewers } from "./topreviewers";
 
 const PERIOD_BASE_FILTER = {
   LAST_30DAYS: { id: "last-30days", label: "last 30 days" },
@@ -19,8 +20,34 @@ async function getContributors(id) {
   return res.json();
 }
 
+async function pullRequestList() {
+  const res = await fetch(
+    "https://api.github.com/repos/Venkadeshkissflow/kf-pr-dashboard/pulls"
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch reviewerInfo");
+  }
+
+  return res.json();
+}
+
+async function getCommentsCount() {
+  const res = await fetch(
+    "https://api.github.com/repos/Venkadeshkissflow/kf-pr-dashboard/pulls/comments"
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch reviewerInfo");
+  }
+
+  return res.json();
+}
+
 export default async function CommonInfo() {
   const contributorsList = await getContributors();
+  const pullRequestsList = await pullRequestList();
+  const commentsCount = await getCommentsCount();
 
   console.log(contributorsList, "contributorsList");
   return (
@@ -37,19 +64,16 @@ export default async function CommonInfo() {
         </select>
       </header>
       {/* <h1>Common info</h1> */}
-      <div className="flex justify-around p-4">
-        <InfoCard
-          title={"Total Contributors"}
-          value={contributorsList.length}
-        />
-        <InfoCard
-          title={"Total Contributors"}
-          value={contributorsList.length}
-        />
-        <InfoCard
-          title={"Total Contributors"}
-          value={contributorsList.length}
-        />
+      <div className="p-4">
+        <div className="flex justify-around p-4">
+          <InfoCard
+            title={"Total Contributors"}
+            value={contributorsList.length}
+          />
+          <InfoCard title={"Total Open prs"} value={pullRequestsList.length} />
+          <InfoCard title={"Total Comments"} value={commentsCount.length} />
+        </div>
+        <TopReviewers />
       </div>
     </>
   );
@@ -57,7 +81,7 @@ export default async function CommonInfo() {
 
 function InfoCard({ title, value }) {
   return (
-    <div className="border-2 rounded-lg min-w-40">
+    <div className="border-2 rounded-lg w-48">
       <div className="flex border-b-2 justify-center font-semibold p-3">
         {title}
       </div>
